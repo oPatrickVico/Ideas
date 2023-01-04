4f05afd3a5eef384a05cc678eb73f2b2504fa1b9
Alan Patrick <alan.patrick.fv@gmail.com>
2023-01-04 15:13:31 -0300

    feat: Add Node component plus some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Node } from './Node/Node';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      Sup mortal! 
+      <Node />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Node/Node.tsx
@@ -0,0 +1,39 @@
+import './Node.scss';
+
+export function Node() {
+  const style: NodeStyle = {
+    width: 500,
+    height: 500,
+    top: 200,
+    left: 500,
+    '--bg-color': 'hsl(190,50%,70%)',
+  };
+
+  return (
+    <article
+      className="Node"
+      style={style}
+      draggable={true}
+      onDrag={(e) => {
+        console.log(e);
+      }}
+    >
+      <h1 className="Node__title">This is the first node!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+        quod ut eius ex repellendus accusantium labore corrupti corporis, ipsum
+        temporibus quasi soluta fuga quo aut dicta reiciendis sit maiores!
+        Quaerat.
+      </p>
+    </article>
+  );
+}
+
+interface NodeStyle {
+  width: number;
+  height: number;
+  top: number;
+  left: number;
+
+  [key: string]: any;
+}
